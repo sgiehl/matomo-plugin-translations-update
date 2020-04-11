33f8526f9886cd1c792b3753cad2cd7e7a26578f
Stefan Giehl <stefangiehl@gmail.com>
2020-04-11 22:01:20 +0200

    Update create-pr.sh

--- create-pr.sh
+++ create-pr.sh
@@ -57,13 +57,10 @@ echo "Translation udpates fetched."
 
 # Check for changes
 cd $GITHUB_WORKSPACE/plugin
-echo "1"
 git add lang/
-echo "2"
 git add "*.json"
-echo "3"
 IFS=$'\n'
-changes=( $(git diff --numstat HEAD | grep -E '([0-9]+)\s+([0-9]+)\s+[a-zA-Z\/]*lang\/([a-z]{2,3}(-[a-z]{2,3})?)\.json' ) )
+changes=( $(git diff --numstat HEAD | grep -E '([0-9]+)\s+([0-9]+)\s+[a-zA-Z\/]*lang\/([a-z]{2,3}(-[a-z]{2,3})?)\.json' ) ) || true
 unset IFS
 echo "Check for new changes."
 
